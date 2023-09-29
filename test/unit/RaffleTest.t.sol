7caa1955af4c20b5618791a005097521289141dc
Aayush Gupta <guptaaayush432@gmail.com>
2023-09-29 22:51:53 +0530

    Created RaffleTest.t.sol

---
+++ test/unit/RaffleTest.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract RaffleTest is Test {
+    Raffle raffle;
+    HelperConfig helperConfig;
+
+    uint256 _entranceFee;
+    uint256 _interval;
+    address _vrfCoordinator;
+    bytes32 _gasLane;
+    uint64 _subscriptionId;
+    uint32 _callbackGasLimit;
+
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+
+    function setUp() external {
+        DeployRaffle deployRaffle = new DeployRaffle();
+        (raffle, helperConfig) = deployRaffle.run();
+
+        (
+            _entranceFee,
+            _interval,
+            _vrfCoordinator,
+            _gasLane,
+            _subscriptionId,
+            _callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
+    }
+
+    function testRaffleInitializesInOpenState() public view {
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+}
