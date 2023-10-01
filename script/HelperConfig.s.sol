6410fb8ac407230e793f8b6edd920e847f9230e4
Aayush Gupta <guptaaayush432@gmail.com>
2023-10-01 23:48:43 +0530

    updated function type

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -15,8 +15,11 @@ contract HelperConfig is Script {
         uint64 _subscriptionId;
         uint32 _callbackGasLimit;
         address _link;
+        uint256 _deployerKey;
     }
 
+    uint256 public constant DEFAULT_ANVIL_KEY =
+        0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
     NetworkConfig public activeNetworkConfig;
 
     constructor() {
@@ -27,7 +30,7 @@ contract HelperConfig is Script {
         }
     }
 
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
         return
             NetworkConfig({
                 _entranceFee: 0.01 ether,
@@ -36,7 +39,8 @@ contract HelperConfig is Script {
                 _gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
                 _subscriptionId: 0,
                 _callbackGasLimit: 500_000, // 500,000 gas
-                _link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+                _link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+                _deployerKey: vm.envUint("PRIVATE_KEY")
             });
     }
 
@@ -65,7 +69,8 @@ contract HelperConfig is Script {
                 _gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
                 _subscriptionId: 0, // our script will add this
                 _callbackGasLimit: 500_000, // 500,000 gas
-                _link: address(link)
+                _link: address(link),
+                _deployerKey: DEFAULT_ANVIL_KEY
             });
     }
 }
